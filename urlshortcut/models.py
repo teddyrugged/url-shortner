52fc2a117313ad6a5fb5cea8d5978d7b9006dfe4
teddyrugged <jakpaetuk@gmail.com>
2022-08-14 22:02:51 +0100

    created model

--- urlshortcut/models.py
+++ urlshortcut/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class Urlshortner(models.Model):
     original_url = models.URLField(max_length=900, blank=True)
     new_url = models.CharField(max_length=80, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return f"Urlshortner: {self.original_url}"
