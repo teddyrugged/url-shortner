3daadca3981512ade1612cda0948488608692b52
teddyrugged <jakpaetuk@gmail.com>
2022-08-14 20:30:46 +0100

    model

--- urlshortcut/models.py
+++ urlshortcut/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    original_url = models.URLField(max_length=900, blank=True)
+    new_url = models.CharField(max_length=80, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
