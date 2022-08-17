3d654a7c5ace06cdd7dbc9b713e0e8d413b61e07
teddyrugged <jakpaetuk@gmail.com>
2022-08-17 23:23:23 +0100

    new

--- urlshortcut/views.py
+++ urlshortcut/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return render (request, 'index.html')
+    return render(request, "index.html")
 
 
 def create(request):
-    if request.method == 'POST':
-        link = request.POST.get('link')
+    if request.method == "POST":
+        link = request.POST.get("link")
         uid = str(uuid.uuid4())[:7]
-        new_url = url(link = link,uuid = uid)
+        new_url = url(link=link, uuid=uid)
         new_url.save()
-        response = HttpResponse(uid) 
+        response = HttpResponse(uid)
+
 
 # Create your views here.
