296ecedaffb246d1eb59e4d0c04ed5491003724e
teddyrugged <jakpaetuk@gmail.com>
2022-08-18 20:22:06 +0100

    add css

--- urlshortcut/urls.py
+++ urlshortcut/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("create", views.create, name="create"),
+    path('', views.index, name = 'index'),
+    path('create', views.create, name = 'create')
 ]
